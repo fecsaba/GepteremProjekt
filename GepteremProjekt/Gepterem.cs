f54e889a4d9f359e5d3e94fd3e4e8347c7883384
fecsaba <nexusegyesulet@gmail.com>
2020-04-21 20:08:09 +0200

    Gepterem

---
+++ GepteremProjekt/Gepterem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GepteremProjekt
+{
+    class Gepterem
+    {
+		private int[,] ertekeles;
+		private int helyDb;
+		private string nev;
+		private int sorDb;
+
+		public int SorDb
+		{
+			get { return sorDb; }
+		}
+
+
+		public int[,] Ertekeles
+		{
+			get { return ertekeles; }
+		}
+
+
+		public int HelyDb
+		{
+			get { return helyDb; }
+		}
+
+		
+
+		public string Nev
+		{
+			get { return nev; }
+		}
+
+		public Gepterem(string nev, int sorDb, int helyDb, int[,] ertekeles)
+		{
+			this.nev = nev;
+			this.sorDb = sorDb;
+			this.helyDb = helyDb;
+			this.ertekeles = ertekeles;
+		}
+
+	}
+}
